847cfc43cbfc76a738c62c2dd997a891f4b0ee27
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-28 01:09:57 +0600

    3 Task is completed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        int counter = 0;
+        for (String arg : args) {
+            try{
+                double buffer;
+                buffer = Double.parseDouble(arg);
+                if(buffer > 0){
+                    counter++;
+                }
+            } catch (NumberFormatException e){
+                return false;
+            }
+        }
+        return counter == args.size();
     }
 }
