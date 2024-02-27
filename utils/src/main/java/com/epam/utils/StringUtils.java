2135b2f2ec3737fd51ec6411c509f974372cf530
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-28 01:08:31 +0600

    2 Task is completed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try{
+            if(str == null){
+                return false;
+            }
+            if(str.startsWith("0")){
+                return false;
+            }
+            double num = Double.parseDouble(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
